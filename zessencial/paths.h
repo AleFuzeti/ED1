305576db00e74b275294cfccebb481ecffbeee6f
AleFuzeti <104146232+AleFuzeti@users.noreply.github.com>
2022-05-02 21:20:20 -0300

    Delete zessencial directory

--- zessencial/paths.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
-
-
-void* createAllPaths();
-char* prepareDir(char* dir);
-char* prepareDoc(char* asread, char* onlyname);
